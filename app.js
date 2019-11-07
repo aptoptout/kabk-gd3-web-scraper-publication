88f6c2f2b43575b3e278a5f246ebb3b3fdc1a990
Apt Opt Out <message@aptoptout.com>
2019-11-07 12:24:21 +0100

    update

--- app.js
+++ app.js
@@ -2,18 +2,23 @@
 var express = require('express');
 var request = require('request');
 var cheerio = require('cheerio');
-var exec = require('child_process').exec;
-var spawn = require('child_process').spawn;
 var fs = require('fs');
 var url = require('url');
+var exec = require('child_process').exec;
+var spawn = require('child_process').spawn;
 
-var port = 9999;
+var Twitter = require('twitter');
+var twitterKeys = require('./secret_keys.js');
+
+var port = 8081;
 var app = express();
 
 var DOWNLOAD_DIR = './';
 
+
 // WIKIPEDIA SCRAPER: access by going to 'localhost:2100/wikipedia'
 app.get('/wikipedia', function(req, res) {
+
 
   var url = "https://en.wikipedia.org/wiki/Phyllotaxis";
 
@@ -27,6 +32,7 @@ app.get('/wikipedia', function(req, res) {
       var $ = cheerio.load(html);
 
       // let's create a javascript object to save our data in
+
       var wiki_data = {
         title: '',
         img: '',
@@ -35,12 +41,50 @@ app.get('/wikipedia', function(req, res) {
 
       // all the content we are looking for are inside a div with the id 'content', let's filter so that the data we are working with is without unnecessary data
       $('#content').filter(function(){
-
         // we can access the properties of our javascript object by writing the name of the object 'dot' and then the name of the property
         wiki_data.title = $(this).find('h1').text();
         wiki_data.img = $(this).find('img').attr('src');
         wiki_data.paragraph = $(this).find('p').first().text();
+      });
 
+      // send the data we've stored in our object back to the browser
+      res.send(wiki_data);
+
+      fs.writeFile('./data/wiki_output.js', "var wiki_output = " + JSON.stringify(wiki_data), function(error){
+        console.log("File is written successfully!");
+      });
+    }
+  });
+});
+
+app.get('/wikipedia', function(req, res) {
+
+
+  var url = "https://en.wikipedia.org/wiki/Phyllotaxis";
+
+  // let's make the http request to the url above using the 'request' dependency
+  request(url, function(error, response, html) {
+
+    // only execute if there's no error
+    if( !error ){
+
+      // we can use the dependency 'cheerio' to traverse the DOM and use jQuery-like selectors and functions
+      var $ = cheerio.load(html);
+
+      // let's create a javascript object to save our data in
+
+      var wiki_data = {
+        title: '',
+        img: '',
+        paragraph: ''
+      };
+
+      // all the content we are looking for are inside a div with the id 'content', let's filter so that the data we are working with is without unnecessary data
+      $('#content').filter(function(){
+        // we can access the properties of our javascript object by writing the name of the object 'dot' and then the name of the property
+        wiki_data.title = $(this).find('h1').text();
+        wiki_data.img = $(this).find('img').attr('src');
+        wiki_data.paragraph = $(this).find('p').first().text();
       });
 
       // send the data we've stored in our object back to the browser
@@ -54,8 +98,39 @@ app.get('/wikipedia', function(req, res) {
 });
 
 app.get('/wikihow', function(){
-  var keywords = ["love", "cry", "hug"];
+
   var urls = [];
+
+  var keywords = ["love", "cry", "hug"];
+
+  keywords[0]; // love
+  keywords[1]; // cry
+  keywords[2]; // hug
+
+  keywords.forEach(function(elem, index){
+    var imageElement = "<img src='" + keywords[index] + "'>"
+
+    // <img src=cry >
+    // <img src='cry' >
+    // document.innerHTML = imageElement;
+  });
+
+
+
+
+  keywords.forEach(function(){
+    console.log(this);
+  });
+
+  for(var index = 1; index > keywords.length; index++) {
+    console.log("HI");
+  }
+
+
+
+
+
+
 
   for(word in keywords) {
 
@@ -227,16 +302,12 @@ var download_file_curl = function(file_url) {
 
 app.get('/twitch', function(req, res){
   request('https://www.youtube.com/watch?v=28HABZJ358g', function(error, response, html){
-
     res.send(html);
-
   });
 });
 
 app.get('/guten', function(req, res) {
-
   var url = 'https://www.gutenberg.org/browse/scores/top';
-
   request(url, function(error, response, html) {
     if (!error) {
 
@@ -260,8 +331,40 @@ app.get('/guten', function(req, res) {
 
     }
   });
+});
+
+var client = new Twitter({
+  consumer_key: twitterKeys.twitter_api_key,
+  consumer_secret: twitterKeys.twitter_secret_key,
+  bearer_token: twitterKeys.twitter_bearer_key
+});
+
+app.get('/twitter', function(req, res) {
+
+  // favorite tweets
+  client.get('favorites/list', function(error, tweets, response) {
+    if(error) throw error;
+    console.log(tweets);    // The favorites.
+    console.log(response);  // Raw response object.
+  });
+
+  //
+  // Stream statuses filtered by keyword
+  // number of tweets per second depends on topic popularity
+  //
+  // client.stream('statuses/filter', {track: 'twitter'},  function(stream) {
+  //   stream.on('data', function(tweet) {
+  //     console.log(tweet.text);
+  //   });
+  //
+  //   stream.on('error', function(error) {
+  //     console.log(error);
+  //   });
+  // });
 
 });
+
+
 
 app.listen(port);
 console.log('Magic happens on port ' + port);
--- data/wiki_output.js
+++ data/wiki_output.js
@@ -1,1 +1,1 @@
-var wiki_output = {"title":"Phyllotaxis","img":"//upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Aloe_polyphylla_1.jpg/275px-Aloe_polyphylla_1.jpg","paragraph":"In botany, phyllotaxis or phyllotaxy is the arrangement of leaves on a plant stem (from Ancient Greek phýllon \"leaf\" and táxis \"arrangement\").[1] Phyllotactic spirals form a distinctive class of patterns in nature.\n"}
+var wiki_output = {"title":"","img":"","paragraph":""}
